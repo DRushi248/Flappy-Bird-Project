b613130aa60fe6efa33e9ecf6ccdbaae3efe72e5
Rushikesh Deshmukh <rushikeshdeshmukh248@gmail.com>
2025-05-19 00:33:39 +0530

    delpoying changes in backend

--- Flappy_Multiplayer/server/index.js
+++ Flappy_Multiplayer/server/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://flappy-bird-project-mebk0zl9q-rushis-projects-69f0fb24.vercel.app/",
         methods: ["GET", "POST"]
     },
 });
